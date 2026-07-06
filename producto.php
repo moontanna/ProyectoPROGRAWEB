a5e26448aaf35068eabc95a469d5b3652dff7eeb
Lisset <Lissetchin@gmail.com>
2026-07-05 19:11:32 -0600

    Listo inciso de consultar producto por clave

--- producto.php
+++ producto.php
@@ -9,7 +9,6 @@ Hannah Kareli Herrera Mondragón
 */
 class Producto
 {
-    
     public $claveProd;
     public $nomProd;
     public $categProd;
@@ -24,17 +23,19 @@ class Producto
 
     public function __construct($claveProd="",$nomProd="",$categProd="",$cantProd="",$precProd="")
     {
+        mysqli_report(MYSQLI_REPORT_OFF);
+
         $this->claveProd=$claveProd;
         $this->nomProd=$nomProd;
         $this->categProd=$categProd;
         $this->cantProd=$cantProd;
         $this->precProd=$precProd;
 
-        $this->Servidor="localhost";
+        $this->Servidor="127.0.0.1";
         $this->Administrador="root";
         $this->BaseDatos="inventario";
 
-        $this->Link=mysqli_connect(
+        $this->Link=@mysqli_connect(
             $this->Servidor,
             $this->Administrador,
             ""
@@ -45,12 +46,24 @@ class Producto
             die("Error al conectar.");
         }
 
-        mysqli_select_db(
-            $this->Link,
-            $this->BaseDatos
-        );
+        mysqli_query($this->Link, "CREATE DATABASE IF NOT EXISTS inventario");
+        
+        mysqli_select_db($this->Link, $this->BaseDatos);
+
+        $tablaSQL = "CREATE TABLE IF NOT EXISTS productos (
+            claveProd VARCHAR(50) PRIMARY KEY,
+            nomProd VARCHAR(100),
+            categProd VARCHAR(100),
+            cantProd INT,
+            precProd DECIMAL(10,2)
+        )";
+        mysqli_query($this->Link, $tablaSQL);
+
+        $verificar = mysqli_query($this->Link, "SELECT * FROM productos WHERE claveProd='1'");
+        if(mysqli_num_rows($verificar) == 0) {
+            mysqli_query($this->Link, "INSERT INTO productos VALUES ('1', 'Reloj de Oro', 'Relojes', '10', '2500.00')");
+        }
+
     }
-
 }
-
 ?>
